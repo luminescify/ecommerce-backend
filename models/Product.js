c9690d637f8a95a01888d047fa23eda5b16ec613
Leah Davis <ileahd96@gmail.com>
2021-10-28 20:42:57 -0400

    Updated Product model with requirements

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,33 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: FontFaceSetLoadEvent,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isNumeric: true,
+    },
+    stock: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isNumeric: true
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
