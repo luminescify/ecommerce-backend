47d4874b415679a8a4b7c147f20f0dffaf70e75c
Leah Davis <ileahd96@gmail.com>
2021-10-28 21:32:54 -0400

    Added empty associations inside index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
 
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
 
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+
+});
+
 
 module.exports = {
   Product,
