de52846d819d1386d7778658fe0a2fe9f4d4bddb
Leah Davis <ileahd96@gmail.com>
2021-10-29 21:32:43 -0400

    Finished building index.js associations

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
+  foreignKey: 'category_id',
 });
 // Categories have many Products
 Category.hasMany(Product, {
@@ -14,11 +14,11 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-
+  foreignKey: 'product_id',
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-
+  foreignKey: 'tag_id'
 });
 
 
